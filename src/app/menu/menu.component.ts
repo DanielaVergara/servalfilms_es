11d1e89271269b16a60602d74a0f58489db29b30
Daniela Vergara <danielavergara@Dani30.local>
2023-11-25 03:06:04 -0500

    feat: test menu

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css']
 })
-export class MenuComponent implements OnInit {
+export class MenuComponent {
+  isMenuOpen = false; // Variable para controlar el estado del menú
 
-  constructor() { }
-
-  ngOnInit(): void {
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen; // Cambia el estado del menú al hacer clic en el botón de hamburguesa
   }
-
 }
