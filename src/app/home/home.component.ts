4a2473bc8a7f103887b9776f8dbe67a4cb61900e
Daniela Vergara <danielavergara@Dani30.local>
2024-09-17 09:48:09 -0500

    feat(home): section one

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,13 +11,13 @@ export class HomeComponent implements AfterViewInit {
   constructor() { }
 
   ngAfterViewInit(): void {
-    gsap.timeline({ repeat: -1, defaults: { duration: 1 } })
-      .fromTo('.gif-background', { opacity: 0 }, { opacity: 1, ease: 'power1.inOut' })
-      .fromTo('.gif-background .gif-image', { rotation: 0.1 }, { rotation: -0.1, ease: 'none', yoyo: true, repeat: 1 }, 0)
-      .to('.gif-background .gif-image', { opacity: 10, ease: 'power2.inOut' }, 4)
+    gsap.timeline({ defaults: { duration: 1 } })
+    .fromTo('.gif-background', { opacity: 0 }, { opacity: 1, ease: 'power1.inOut' })
+    .fromTo('.gif-background .gif-image', { rotation: 0.1 }, { rotation: -0.1, ease: 'none', yoyo: true, repeat: 1 }, 0)
+    .to('.gif-background .gif-image', { opacity: 1, ease: 'power2.inOut' }, 4)
       
       .from('.title', { duration: 0.5, opacity: 1, y: -40, ease: 'power3.out' })
-      .from('.subtitle', { duration: 0.5, opacity: 1, y: 50, ease: 'power3.out', delay: 0.5 })
+      .from('.subtitle', { duration: 0.5, opacity: 1, y: 50, ease: 'power3.out', delay: 0.4 })
       .from('.icon', { duration: 0.5, opacity: 0, scale: 0.5, ease: 'power3.out' }, 2); 
   }
   
