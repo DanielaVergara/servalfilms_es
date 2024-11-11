e7f67ad7ab7bd3e06a9a45f7cbff3830952f0785
Daniela Vergara <danielavergara@Dani30.local>
2024-11-11 13:42:42 -0500

    feat(footer): logo

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ElementRef, ChangeDetectorRef } from '@angular/core';
+const rough = require('roughjs/bundled/rough.cjs');
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
-export class FooterComponent implements OnInit {
+export class FooterComponent implements AfterViewInit {
 
-  constructor() { }
+  @ViewChild('lineLeft') lineLeft!: ElementRef;
+  @ViewChild('lineRight') lineRight!: ElementRef;
 
-  ngOnInit(): void {
+  constructor(private cdr: ChangeDetectorRef) { }
+
+  ngAfterViewInit(): void {
+    // Asegurarse de que Angular haya completado la detección de cambios
+    this.cdr.detectChanges(); 
+    this.drawRoughLines();
   }
 
+  drawRoughLines() {
+    // Verificar que los elementos estén presentes
+    if (this.lineLeft && this.lineRight) {
+      // Crear el "canvas" de RoughJS para los divs
+      const rcLeft = rough.canvas(this.lineLeft.nativeElement);
+      const rcRight = rough.canvas(this.lineRight.nativeElement);
+
+      // Dibujar las líneas en los divs
+      rcLeft.line(10, 10, 300, 10, { stroke: 'orange', strokeWidth: 3 });
+      rcRight.line(10, 10, 300, 10, { stroke: 'orange', strokeWidth: 3 });
+    }
+  }
 }
